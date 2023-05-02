cd3798f642fc43ad4ae1b9d8aa8fd4b90324e963
Cauí Davis Matos de Oliveira <98659275+cauidavismatosdeoliveira@users.noreply.github.com>
2023-05-02 14:02:50 -0300

    commit

---
+++ diminuirimagem.py
@@ -0,0 +1,20 @@
+import cv2
+
+# Carrega a imagem
+img = cv2.imread('home.jpg')
+
+# Define o fator de redução da imagem
+fator_reducao = 0.2
+
+# Aplica a redução de tamanho
+nova_altura = int(img.shape[0] * fator_reducao)
+nova_largura = int(img.shape[1] * fator_reducao)
+img_reduzida = cv2.resize(img, (nova_largura, nova_altura))
+
+# Exibe a imagem original e a imagem reduzida
+cv2.imshow("Imagem Original", img)
+cv2.imshow("Imagem Reduzida", img_reduzida)
+cv2.imwrite('home1.jpg', img_reduzida)
+# Espera por uma tecla ser pressionada para fechar as janelas
+cv2.waitKey(0)
+cv2.destroyAllWindows()
